8e6651e48d9fc1de6580497582f4fceb471d9c70
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-05-31 20:50:54 +0200

    added some extra icons in home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,4 +9,6 @@ export class HomeComponent {
 numbers:number[]=[1,2,3,4,5,6,7,8]
 numbers1:number[]=[1,2,3]
 name:string[]=['S','P','A','C','E',' ','A','G','E','N','C','Y']
+images:string[]= ['../../../assets/Images/icons/team.svg','../../../assets/Images/icons/rocket.svg.png',
+'../../../assets/Images/icons/fuel-filling.svg','../../../assets/Images/icons/atoms-science.svg']
 }
