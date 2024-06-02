5d967bc093319a85a667302df9de5c215362c682
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-02 09:49:29 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AuthFormsComponent } from './components/auth-forms/auth-forms.component';
+import { ItinerariComponent } from './components/itinerari/itinerari.component';
+import { SpaceCrewComponent } from './components/space-crew/space-crew.component';
+import { MezziComponent } from './components/mezzi/mezzi.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 const routes: Routes = [
   {
     path:"",
+    component:HomeComponent
+  },
+  {
+    path:"auth",
+    component:AuthFormsComponent
+  },
+  {
+    path:"itinerari",
+    component:ItinerariComponent
+  },
+  {
+    path:"spaceCrew",
+    component:SpaceCrewComponent
+  },
+  {
+    path:"mezzi",
+    component:MezziComponent
+  },
+  {
+    path:"about",
+    component:AboutUsComponent
+  },
+  {
+    path:"**",
     component:HomeComponent
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AuthFormsComponent } from './components/auth-forms/auth-forms.component';
+import { ItinerariComponent } from './components/itinerari/itinerari.component';
+import { SpaceCrewComponent } from './components/space-crew/space-crew.component';
+import { MezziComponent } from './components/mezzi/mezzi.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    AuthFormsComponent,
+    ItinerariComponent,
+    SpaceCrewComponent,
+    MezziComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
