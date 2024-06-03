ed79191681fe6a94dda59e72257ae41e2bd58033
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-03 09:46:14 +0200

    doing auth component

--- src/app/components/auth-forms/auth-forms.component.ts
+++ src/app/components/auth-forms/auth-forms.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth-forms',
   templateUrl: './auth-forms.component.html',
   styleUrls: ['./auth-forms.component.scss']
 })
-export class AuthFormsComponent {
+export class AuthFormsComponent  implements OnInit{
 
+section:string=''
+
+constructor(){}
+
+
+ngOnInit(): void {
+    this.section='login'
+}
 }
