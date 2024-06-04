349335fcd0b6944b55bd5be014a5c7b90c5b03b1
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-06-04 11:10:08 +0200

    bringing validation to another level

--- src/app/components/auth-forms/auth-forms.component.ts
+++ src/app/components/auth-forms/auth-forms.component.ts
@@ -22,12 +22,12 @@ ngOnInit(): void {
     this.section='login'
 
 this.loginForm= new FormGroup({
-  email:new FormControl('', [Validators.required,Validators.email,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)]),
-  password: new FormControl('',[Validators.required,Validators.minLength(6)])
+  email:new FormControl('', [Validators.required]),
+  password: new FormControl('',[Validators.required])
 })
 
 this.signupForm = new FormGroup({
-  email:new FormControl('', [Validators.required,Validators.email,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)]),
+  email:new FormControl('', [Validators.required,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)]),
   password: new FormControl('',[Validators.required,Validators.minLength(6)]),
   nome: new FormControl('',Validators.required),
   cognome: new FormControl('',Validators.required),
@@ -47,11 +47,14 @@ this.authService.logIn
 ).subscribe({
   next:(success:any)=>{
 console.log(success)
+this.loginFormError=""
   },
   error:(error:any)=>{
 this.loginFormError="Qualcosa è successo nell'invio della richiesta."
 if(error&&error.error.message){
   this.loginFormError=error.error.message
+}else if(error&&error.error.messageList){
+  this.loginFormError=error.error.messageList[0]
 }
   },
   complete:()=>{
@@ -76,11 +79,15 @@ signUp(){
   ).subscribe({
     next:(success:any)=>{
   console.log(success)
+  this.signupFormError=""
+  this.section='login'
     },
     error:(error:any)=>{
   this.signupFormError="Qualcosa è successo nell'invio della richiesta."
   if(error&&error.error.message){
     this.signupFormError=error.error.message
+  }else if (error&&error.error.messageList){
+    this.signupFormError=error.error.messageList[0]
   }
     },
     complete:()=>{
