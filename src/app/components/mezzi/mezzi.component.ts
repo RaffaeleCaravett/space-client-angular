be01d23a6f12b0bf92cc8a7bdd9eba897bf0dd73
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-01 21:37:01 +0200

    start doing mezzi component

--- src/app/components/mezzi/mezzi.component.ts
+++ src/app/components/mezzi/mezzi.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mezzi',
   templateUrl: './mezzi.component.html',
   styleUrls: ['./mezzi.component.scss']
 })
-export class MezziComponent {
+export class MezziComponent implements OnInit{
+  ngOnInit(): void {
+localStorage.setItem('location','mezzi')  }
 
 }
