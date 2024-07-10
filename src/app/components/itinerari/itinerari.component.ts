7527213609921053217ca7e086b15acf6b136c3d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-10 17:03:30 +0200

    now doing itinerari

--- src/app/components/itinerari/itinerari.component.ts
+++ src/app/components/itinerari/itinerari.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackgroundService } from 'src/app/shared/services/background-service';
 
 @Component({
   selector: 'app-itinerari',
   templateUrl: './itinerari.component.html',
   styleUrls: ['./itinerari.component.scss']
 })
-export class ItinerariComponent {
+export class ItinerariComponent implements OnInit{
+background:string =''
+
+constructor(private backgroundService:BackgroundService){
+  this.backgroundService.bgClass.subscribe((bg:string)=>{
+    this.background=bg
+  })
+}
+
+ngOnInit(): void {
+localStorage.setItem('location','itinerari')
+}
 
 }
