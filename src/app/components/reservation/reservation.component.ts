f57869c6a3f3ab80ec1ac0bcbf4379a8519bdf32
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-16 21:31:36 +0200

    now doing form to add reservations

--- src/app/components/reservation/reservation.component.ts
+++ src/app/components/reservation/reservation.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.scss']
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit{
+reservationForm!:FormGroup
 
+ngOnInit(): void {
+  throw new Error('Method not implemented.');
+}
 }
