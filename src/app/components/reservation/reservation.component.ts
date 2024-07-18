ca3b5d7ee34d0fac42a6b889f8e4bdf5b4bb665c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-18 21:56:44 +0200

    setted validators

--- src/app/components/reservation/reservation.component.ts
+++ src/app/components/reservation/reservation.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 
@@ -11,20 +11,20 @@ import { ToastrService } from 'ngx-toastr';
 export class ReservationComponent implements OnInit{
 reservationForm!:FormGroup
 user:any
-
+package:any
 constructor(private dialogRef: MatDialogRef<ReservationComponent>, @Inject(MAT_DIALOG_DATA) public data: any , private toastr:ToastrService) {
 }
 ngOnInit(): void {
-  console.log(this.data[1])
 this.user=JSON.parse(this.data[0])
+this.package=this.data[1]
 this.reservationForm= new FormGroup({
-  nome: new FormControl(this.user.nome||''),
-  cognome: new FormControl(this.user.cognome||''),
-  eta: new FormControl(this.user.eta||''),
-  email: new FormControl(this.user.email||''),
-  nomePianeta: new FormControl(),
-  Da: new FormControl(),
-  A: new FormControl(),
+  nome: new FormControl(this.user.nome||'',Validators.required),
+  cognome: new FormControl(this.user.cognome||'',Validators.required),
+  eta: new FormControl(this.user.eta||'',Validators.required),
+  email: new FormControl(this.user.email||'',Validators.required),
+  nomePianeta: new FormControl(this.package.pianetas[0].nome||'',Validators.required),
+  Da: new FormControl(this.package.da||'',Validators.required),
+  A: new FormControl(this.package.a||'',Validators.required),
 })
 }
 }
