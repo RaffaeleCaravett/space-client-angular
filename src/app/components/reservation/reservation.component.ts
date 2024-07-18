1001c727db2b5b4d92227455c9e680905bf6e585
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-07-18 21:43:42 +0200

    doing form ts side

--- src/app/components/reservation/reservation.component.ts
+++ src/app/components/reservation/reservation.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 
@@ -10,11 +10,20 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ReservationComponent implements OnInit{
 reservationForm!:FormGroup
-
+user:any
 
 constructor(private dialogRef: MatDialogRef<ReservationComponent>, @Inject(MAT_DIALOG_DATA) public data: any , private toastr:ToastrService) {
 }
 ngOnInit(): void {
-console.log(this.data)
+this.user=JSON.parse(this.data[0])
+this.reservationForm= new FormGroup({
+  nome: new FormControl(),
+  cognome: new FormControl(),
+  eta: new FormControl(),
+  email: new FormControl(),
+  nomePianeta: new FormControl(),
+  Da: new FormControl(),
+  A: new FormControl(),
+})
 }
 }
